08fd5e34ece4e62f4f9ea60118b0d48a051cd22d
Edleson Duarte <edleson@localdomain>
2025-08-21 16:05:59 -0300

    feat: Configure example component + testing

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -1,6 +1,12 @@
+import Image from 'next/image'
+
 const Main = () => (
-  <main>
-    <h1>React Avançado</h1>
+  <main className="flex flex-col justify-center items-center h-screen gap-5">
+    <h1 className="flex text-9xl font-bold font-primary">Kode3 Tech</h1>
+    <p className="text-4xl font-secondary">
+      Transformamos ideias em soluções eficientes e inovadoras.
+    </p>
+    <Image src="logo.svg" alt="Kode3 Tech" width={200} height={200} priority />
   </main>
 )
 
--- src/components/Main/test.tsx
+++ src/components/Main/test.tsx
@@ -9,7 +9,28 @@ describe('<Main />', () => {
 
     // busca o elemento e verifica a existência dele
     expect(
-      screen.getByRole('heading', { name: /react avançado/i })
+      screen.getByRole('heading', { name: /kode3 tech/i })
+    ).toBeInTheDocument()
+  })
+
+  it('should render the logo', () => {
+    // renderiza o component
+    render(<Main />)
+
+    // busca o elemento pela role e depois checa o src
+    const logo = screen.getByRole('img', { name: /kode3 tech/i })
+    expect(logo).toHaveAttribute('src', expect.stringContaining('logo.svg'))
+  })
+
+  it('should render the description', () => {
+    // renderiza o component
+    render(<Main />)
+
+    // busca o elemento e verifica a existência dele
+    expect(
+      screen.getByText(
+        'Transformamos ideias em soluções eficientes e inovadoras.'
+      )
     ).toBeInTheDocument()
   })
 })
